91e2780c3c5c028c0a153b27a67f32417e52f9ef
Zack Larsen <zacklarsen1@gmail.com>
2019-12-04 09:44:50 -0700

    didn't quite figure out the math behind it.

--- sumAndProduct.playground/Contents.swift
+++ sumAndProduct.playground/Contents.swift
@@ -1,5 +1,18 @@
 import UIKit
 
 func sumAndProduct(_ sum: UInt, _ product: UInt) -> [UInt] {
-    
+    let result = [sum, product]
+    print("[\(result)]")
+    return result
 }
+
+func sum(x: Int, y: Int) -> Int {
+    return x + y
+}
+
+func product(x: Int, y: Int) -> Int {
+    return x * y
+}
+sumAndProduct(6, 9)
+
+
